352991777c30985d408b50ce7233eabb990310c9
CHANSE HORTON <chansehorton@yahoo.com>
2017-03-07 12:28:37 -0700

    now stores notes in db by user, and can toggle opacity of note

--- js/background.js
+++ js/background.js
@@ -1,10 +1,58 @@
 'use strict'
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("adding yellow box");
-
-  chrome.tabs.executeScript({
-    file: '/js/toggle.js'
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, { toggle: true }, function(response) {
+      console.log(response);
+    });
   });
 
 });
+
+getUserInfo(true);
+
+function getUserInfo (interactive) {
+  const xmlhttp = new XMLHttpRequest();
+  let retry = true;
+  let access_token;
+
+  getToken();
+
+  /**
+   * Request the Auth Token
+   */
+  function getToken() {
+    chrome.identity.getAuthToken( { 'interactive': interactive }, function (token) {
+      if (chrome.runtime.lastError) {
+        console.log( "ERROR! " + chrome.runtime.lastError.message );
+        return;
+      }
+      if (typeof token != 'undefined') {
+        access_token = token;
+        sendRequest();
+      }
+
+    });
+  }
+
+  function sendRequest() {
+    xmlhttp.open('GET', 'https://www.googleapis.com/userinfo/v2/me' );
+    xmlhttp.setRequestHeader('Authorization','Bearer ' + access_token );
+    xmlhttp.onload = requestComplete;
+    xmlhttp.send();
+  }
+
+  function requestComplete() {
+    if (this.status == 401 && retry) {
+      retry = false; // only retry once
+      console.log( "Request failed, retrying... " + this.response );
+    } else {
+      console.log( "Request completed. User Info: " + this.response );
+      const userInfo = JSON.parse( this.response );
+
+      chrome.storage.local.set({"userId": userInfo.id.toString()}, () => {
+        
+      });
+    }
+  }
+}
